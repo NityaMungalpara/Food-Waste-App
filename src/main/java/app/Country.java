fc005f1f75d5b830b17681e2fe4a81e60ce3c754
Nitya Mungalpara <s4057052@student.rmit.edu.au>
2024-06-20 12:22:41 +1000

    Changes in CountryLossEvent

--- src/main/java/app/Country.java
+++ src/main/java/app/Country.java
@@ -18,6 +18,9 @@ public class Country {
    public String lossStartYear;
    public String loss_end_year;
    public String loss_difference;
+   public String activity;
+   public String food_supply_stage;
+   public String cause_of_loss;
    /**
     * Create a Country and set the fields
     */
--- src/main/java/app/PageST2A.java
+++ src/main/java/app/PageST2A.java
@@ -109,25 +109,151 @@ public class PageST2A implements Handler {
                 <div class='form-group'>
                     <label for='country_drop'>Select the Country(s):</label>
                     <div>
-                        <input type='checkbox' name='country_drop' value='Burundi'>Burundi<br>
-                        <input type='checkbox' name='country_drop' value='Cameroon'>Cameroon<br>
-                        <input type='checkbox' name='country_drop' value='Myanmar'>Myanmar<br>
+                        <input type='checkbox' name='country_drop' value='Africa'>Africa
+                        <input type='checkbox' name='country_drop' value='Algeria'>Algeria
+                        <input type='checkbox' name='country_drop' value='Angola'>Angola
+                        <input type='checkbox' name='country_drop' value='Argentina'>Argentina
+                        <input type='checkbox' name='country_drop' value='Argmenia'>Armenia
+                        <input type='checkbox' name='country_drop' value='Australia'>Australia
+                        <input type='checkbox' name='country_drop' value='Australia and New Zealend'>Australia and New Zealand
+                        <input type='checkbox' name='country_drop' value='Azerbaijan'>Azerbaijan
+                        <input type='checkbox' name='country_drop' value='Bahrain'>Bahrain
+                        <input type='checkbox' name='country_drop' value='Bangladesh'>Bangladesh
+                        <input type='checkbox' name='country_drop' value='Benin'>Benin
+                        <input type='checkbox' name='country_drop' value='Bolivia (Plurinational State of)'>Bolivia (Plurinational State of)
+                        <input type='checkbox' name='country_drop' value='Botswana'>Botswana
+                        <input type='checkbox' name='country_drop' value='Brazil'>Brazil
+                        <input type='checkbox' name='country_drop' value='Burkina Faso'>Burkina Faso
+                        <input type='checkbox' name='country_drop' value='Burundi'>Burundi
+                        <input type='checkbox' name='country_drop' value='Cote d'Ivoire'>Cote d'Ivoire
                     </div>
                 </div>
                 <div class='form-group'>
                     <label for='start_year_drop'>Select the Start Year:</label>
                     <select id='start_year_drop' name='start_year_drop'>
+                        <option>1966</option>
+                        <option>1967</option>
+                        <option>1968</option>
+                        <option>1969</option>
+                        <option>1970</option>
+                        <option>1971</option>
+                        <option>1972</option>
+                        <option>1973</option>
+                        <option>1974</option>
+                        <option>1975</option>
+                        <option>1976</option>
+                        <option>1977</option>
+                        <option>1978</option>
+                        <option>1979</option>
+                        <option>1980</option>
+                        <option>1981</option>
+                        <option>1982</option>
+                        <option>1983</option>
+                        <option>1984</option>
+                        <option>1985</option>
+                        <option>1986</option>
+                        <option>1987</option>
+                        <option>1988</option>
+                        <option>1989</option>
+                        <option>1990</option>
+                        <option>1991</option>
+                        <option>1992</option>
+                        <option>1993</option>
+                        <option>1994</option>
+                        <option>1995</option>
+                        <option>1996</option>
+                        <option>1997</option>
+                        <option>1998</option>
+                        <option>1999</option>
+                        <option>2000</option>
                         <option>2001</option>
                         <option>2002</option>
                         <option>2003</option>
+                        <option>2004</option>
+                        <option>2005</option>
+                        <option>2006</option>
+                        <option>2007</option>
+                        <option>2008</option>
+                        <option>2009</option>
+                        <option>2010</option>
+                        <option>2011</option>
+                        <option>2012</option>
+                        <option>2013</option>
+                        <option>2014</option>
+                        <option>2015</option>
+                        <option>2016</option>
+                        <option>2017</option>
+                        <option>2018</option>
+                        <option>2019</option>
+                        <option>2020</option>
+                        <option>2021</option>
+                        <option>2022</option>
+                        <option>2023</option>
+                        <option>2024</option>
                     </select>
                 </div>
                 <div class='form-group'>
                     <label for='end_year_drop'>Select the End Year:</label>
                     <select id='end_year_drop' name='end_year_drop'>
+                        <option>1966</option>
+                        <option>1967</option>
+                        <option>1968</option>
+                        <option>1969</option>
+                        <option>1970</option>
+                        <option>1971</option>
+                        <option>1972</option>
+                        <option>1973</option>
+                        <option>1974</option>
+                        <option>1975</option>
+                        <option>1976</option>
+                        <option>1977</option>
+                        <option>1978</option>
+                        <option>1979</option>
+                        <option>1980</option>
+                        <option>1981</option>
+                        <option>1982</option>
+                        <option>1983</option>
+                        <option>1984</option>
+                        <option>1985</option>
+                        <option>1986</option>
+                        <option>1987</option>
+                        <option>1988</option>
+                        <option>1989</option>
+                        <option>1990</option>
+                        <option>1991</option>
+                        <option>1992</option>
+                        <option>1993</option>
+                        <option>1994</option>
+                        <option>1995</option>
+                        <option>1996</option>
+                        <option>1997</option>
+                        <option>1998</option>
+                        <option>1999</option>
+                        <option>2000</option>
                         <option>2001</option>
                         <option>2002</option>
                         <option>2003</option>
+                        <option>2004</option>
+                        <option>2005</option>
+                        <option>2006</option>
+                        <option>2007</option>
+                        <option>2008</option>
+                        <option>2009</option>
+                        <option>2010</option>
+                        <option>2011</option>
+                        <option>2012</option>
+                        <option>2013</option>
+                        <option>2014</option>
+                        <option>2015</option>
+                        <option>2016</option>
+                        <option>2017</option>
+                        <option>2018</option>
+                        <option>2019</option>
+                        <option>2020</option>
+                        <option>2021</option>
+                        <option>2022</option>
+                        <option>2023</option>
+                        <option>2024</option>
                     </select>
                 </div>
                 <div class='form-group'>
@@ -139,7 +265,7 @@ public class PageST2A implements Handler {
                     </div>
                 </div>
                 <div class='form-group'>
-                    <label for='order_drop'>Select the Start Year:</label>
+                    <label for='order_drop'>Select the Order:</label>
                     <select id='order_drop' name='order_drop'>
                         <option>Ascending</option>
                         <option>Descending</option>
@@ -231,7 +357,7 @@ public class PageST2A implements Handler {
         
         // Add HTML for the movies list
         html = html + "<table class = 'content-table' border = '1'>";
-        html = html + "<tr class = 'heading'><th>Country</th><th>Start Year</th><th>Start Year Loss Percentage</th><th>End Year</th><th>End Year Loss Percentage</th><th>Loss Percent Difference</th></tr>";
+        html = html + "<tr class = 'heading'><th>Country</th><th>Start Year</th><th>Start Year Loss Percentage</th><th>End Year</th><th>End Year Loss Percentage</th><th>Loss Percent Difference</th><th>Activity</th><th>Food Supply Stage</th><th>Cause Of Loss</th></tr>";
 
         for (int i = 0; i < lossPercentages.size(); i += 3) {
             String country = lossPercentages.get(i);
@@ -247,6 +373,12 @@ public class PageST2A implements Handler {
             html = html + "<td>" + endYear + "</td>";
             html = html + "<td>" + loss_end_year + "</td>";
             html = html + "<td>" + loss_difference + "</td>";
+            String activity="";
+            html = html + "<td>" + activity + "</td>";
+            String food_supply_stage="";
+            html = html + "<td>" + food_supply_stage + "</td>";
+            String cause_of_loss="";
+            html = html + "<td>" + cause_of_loss + "</td>";
             html = html + "</tr>";
         } 
 
@@ -275,27 +407,33 @@ public class PageST2A implements Handler {
                            " FROM food_loss " +
                            " WHERE country IN (" + countriesTogether + ") " +
                            " AND year = '" + startYear + "' " +
-                           " GROUP BY country) t1 " +
+                           " GROUP BY country,year) t1 " +
                            "JOIN " +
                            "(SELECT SUM(loss_percentage) AS loss2, country " +
                            " FROM food_loss " +
                            " WHERE country IN (" + countriesTogether + ") " +
                            " AND year = '" + endYear + "' " +
-                           " GROUP BY country) t2 ON t1.country = t2.country";
-    
+                           " GROUP BY country,year) t2 ON t1.country = t2.country";
+                            //" AND " + "SELECT activity, food_supply_stage, cause_of_loss FROM food_loss";
+            //            
             // Execute the query
             ResultSet results = statement.executeQuery(query);
-    
-            // Process the results
+
             while (results.next()) {
                 String country = results.getString("country");
                 String loss_start_year = results.getString("loss_start_year");
                 String loss_end_year = results.getString("loss_end_year");
-    
-                // Add country and loss percentages to the list
+                // String activity = results.getString("activity"); // Retrieve activity
+                // String food_supply_stage = results.getString("food_supply_stage"); // Retrieve food_supply_stage
+                // String cause_of_loss = results.getString("cause_of_loss"); // Retrieve cause_of_loss
+            
+                // Add country, loss percentages, and additional fields to the list
                 countryData.add(country);
                 countryData.add(loss_start_year);
                 countryData.add(loss_end_year);
+                // countryData.add(activity);
+                // countryData.add(food_supply_stage);
+                // countryData.add(cause_of_loss);
             }
     
             // Close the statement and connection
@@ -311,9 +449,9 @@ public class PageST2A implements Handler {
                 System.err.println(e.getMessage());
             }
         }
-    
         return countryData;
     }
+
     
     public ArrayList<Student> getAllStudents() {
         // Create the ArrayList of Country objects to return
