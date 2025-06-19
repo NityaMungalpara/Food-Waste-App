3a2ad774b370c5d8359298ff7e38be52ac028b6f
Nitya Mungalpara <s4057052@student.rmit.edu.au>
2025-06-19 20:25:32 +1000

    Final changes to host the website

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -6,39 +6,42 @@ import io.javalin.core.util.RouteOverviewPlugin;
 /**
  * Main Application Class.
  * <p>
- * Running this class as regular java application will start the 
- * Javalin HTTP Server and our web application.
- *
- * @author Timothy Wiley, 2023. email: timothy.wiley@rmit.edu.au
- * @author Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
- * @author Halil Ali, 2024. email: halil.ali@rmit.edu.au
+ * Running this class as a regular Java application will start the
+ * Javalin HTTP Server and the web application.
+ * 
+ * Make sure to use a dynamic port if deploying to cloud services like Railway.
+ * 
+ * Authors:
+ * - Timothy Wiley, 2023. email: timothy.wiley@rmit.edu.au
+ * - Santha Sumanasekara, 2021. email: santha.sumanasekara@rmit.edu.au
+ * - Halil Ali, 2024. email: halil.ali@rmit.edu.au
  */
 
 public class App {
 
-    public static final int         JAVALIN_PORT    = 7001;
-    public static final String      CSS_DIR         = "css/";
-    public static final String      IMAGES_DIR      = "images/";
+    // Use PORT from environment (e.g., Railway or Heroku), fallback to 7001
+    public static final int JAVALIN_PORT =
+        Integer.parseInt(System.getenv().getOrDefault("PORT", "7001"));
+
+    public static final String CSS_DIR = "css/";
+    public static final String IMAGES_DIR = "images/";
 
     public static void main(String[] args) {
-        // Create our HTTP server and listen in port 7000
+        // Create and start Javalin server
         Javalin app = Javalin.create(config -> {
             config.registerPlugin(new RouteOverviewPlugin("/help/routes"));
-            
-            // Uncomment this if you have files in the CSS Directory
-            config.addStaticFiles(CSS_DIR);
 
-            // Uncomment this if you have files in the Images Directory
+            // Serve static files
+            config.addStaticFiles(CSS_DIR);
             config.addStaticFiles(IMAGES_DIR);
         }).start(JAVALIN_PORT);
 
-
-        // Configure Web Routes
+        // Configure routes
         configureRoutes(app);
     }
 
     public static void configureRoutes(Javalin app) {
-        // All webpages are listed here as GET pages
+        // GET routes (web pages)
         app.get(PageIndex.URL, new PageIndex());
         app.get(PageMission.URL, new PageMission());
         app.get(PageST2A.URL, new PageST2A());
@@ -47,14 +50,11 @@ public class App {
         app.get(PageST3B.URL, new PageST3B());
         app.get(PageReference.URL, new PageReference());
 
-        // Add / uncomment POST commands for any pages that need web form POSTS
+        // POST routes (form submissions)
         app.post(PageIndex.URL, new PageIndex());
-        //app.post(PageMission.URL, new PageMission());
         app.post(PageST2A.URL, new PageST2A());
         app.post(PageST2B.URL, new PageST2B());
         app.post(PageST3A.URL, new PageST3A());
         app.post(PageST3B.URL, new PageST3B());
-        // app.post(PageReference.URL, new PageReference());
     }
-
 }
